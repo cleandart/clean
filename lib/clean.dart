d861010fee6a5a563330a813ba1b727411f132ba
Samuel Hapak <samuel.hapak@gmail.com>
2013-11-11 15:54:26 +0100

    Add requirements to publish on pub.

---
+++ lib/clean.dart
@@ -0,0 +1,8 @@
+// Copyright (c) 2013, the Clean project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/**
+ * This is just meta-package for clean.dart framework.
+ */
+library clean;
